b0aab5552485c779f5b6e08ef09649c8102b139f
knuxus <knuxus@gmail.com>
2013-06-25 14:36:19 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carvizy::Application.config.secret_token = '06e3f8dc3b6e1584c4c7c9948bb810b384700b60ef4e3639e1b6397a6865a55e8678a22a19b4f9f40d297884d92de95e6e61a7f14b182f98b0d9519e84f90203'
