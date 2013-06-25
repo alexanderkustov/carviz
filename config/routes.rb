36ccc3159c54012910d3532944475999fbd90ea9
knuxus <knuxus@gmail.com>
2013-06-25 16:00:07 +0100

    added some styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Carvizy::Application.routes.draw do
+  
   authenticated :user do
     root :to => 'home#index'
   end
   
   root :to => "home#index"
-  
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
 end
