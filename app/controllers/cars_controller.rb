900bdc76eb230044f7c8f543ab1de2db334c08e0
knuxus <knuxus@gmail.com>
2013-06-27 11:10:51 +0100

    show corrigido, falta edit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,7 +13,7 @@ class CarsController < ApplicationController
   # GET /cars/1
   # GET /cars/1.json
   def show
-    @car = Car.find(params[:id])
+    @car = Car.find(params[:user_id])
 
     respond_to do |format|
       format.html # show.html.erb
